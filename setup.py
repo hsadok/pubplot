932f05da4563316256c2866cbb2bfb4c407efed7
Hugo Sadok <hugombarreto@gmail.com>
2018-07-28 22:02:13 -0300

    Updating to version 0.1.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='pubplot',
-    version='0.1.1',
+    version='0.1.4',
     description='Seamless LaTeX and Matplotlib integration for publication plots',
     long_description=readme,
     packages=find_packages(),
